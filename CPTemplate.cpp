e74c42ddc8a2e8e00b9d68f24b2fc24b663a0d1d
tuan2.le <tuan2.le@lge.com>
2021-03-19 09:19:12 +0700

    update template C++

--- CPTemplate.cpp
+++ CPTemplate.cpp
@@ -4,37 +4,81 @@
 using namespace std;
 
 /****** Template of MACRO/CONSTANT *****/
-#define INF INT_MAX
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<ii> vii;
+
 #define fi first
 #define se second
 #define pb push_back
+#define pf push_front
 #define mp make_pair
-#define all(x) (x).begin(), (x).end()   //Forward traversal
-#define rall(x) (x).rbegin, (x).rend()  //reverse traversal
 #define present(c, x) ((c).find(x) != (c).end())
-#define sz(x) (x.size())
-#define rep(i, from, end) for (int i = from; i < end; i++)
-#define REP(i, from, end) for (int i = from; i <= end; i++)
-#define Trav(it, x) for (auto it : x)
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
-typedef map<int, int> mii;
-typedef map<int, vector<int>> mivi;
+#define Rep(i, n) for (int i = 0; i < (n); ++i)
+#define Repd(i, n) for (int i = (n)-1; i >= 0; --i)
+#define For(i, a, b) for (int i = (a); i <= (b); ++i)
+#define Ford(i, a, b) for (int i = (a); i >= (b); --i)
+#define Trav(i,v) for(__typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
+#define Travd(i,v) for(__typeof((v).rbegin()) i = (v).rbegin(); i != (v).rend(); ++i)
+#define ms(a, x) memset(a, x, sizeof(a))
+#define Val(a, b) typeof(b) a(b)
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin, (x).rend()  //reverse traversal
+#define ms(a,x) memset(a, x, sizeof(a))
+#define sz(a) ((int)(a).size())
+#define last(a) (a)[sz(a)-1]
+
+#define Sort(a) sort(all(a))
+#define GSort(a) sort(all(a), greater<typeof(*((x).begin()))>()>)
+#define Unique(v) Sort(v); (v).resize(unique(all(v)) - (v).begin())
+#define Enum(v) Trav(it, (v)) cout << *it << " "; cout << endl
+
+template<class F, class T> T convert(F a, int p = -1) { stringstream ss; if (p >= 0) ss << fixed << setprecision(p); ss << a; T r; ss >> r; return r; }
+template<class T> T gcd(T a, T b){ T r; while (b != 0) { r = a % b; a = b; b = r; } return a;} //UCLN
+template<class T> T lcm(T a, T b) { return a / gcd(a, b) * b; } //BCNN
+template<class T> T sqr(T x) { return x * x; }
+template<class T> T cube(T x) { return x * x * x; }
+template<class T> int getbit(T s, int i) { return (s >> i) & 1; }
+template<class T> T onbit(T s, int i) { return s | (T(1) << i); }
+template<class T> T offbit(T s, int i) { return s & (~(T(1) << i)); }
+template<class T> int cntbit(T s) { return s == 0 ? 0 : cntbit(s >> 1) + (s & 1); }
+
+const ld PI = acos(-1.0);
+const ld EPS = 1e-9;
+const int dr[] = {-1, 0, +1, 0};
+const int dc[] = {0, +1, 0, -1};
+const int INF = (int)1e9 + 5;
+const ll LINF = (ll)1e16 + 5;
+const ll MOD = (ll)1e9 + 7;
+
+void readInput() {
+}
+
+void solver() {
+}
 
 int main() {
+    // For Cin/Cout
+    // ios::sync_with_stdio(false);
+    // cin.tie(NULL); cout.tie(NULL);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
-    freopen("error.txt", "w", stderr);
     freopen("output.txt", "w", stdout);
 #endif
 
-    int t = 1;
+    int T = 1;
     // scanf("%d", &t);
-    while (t--) {
-        // printf("Solution %d\n", t);
-
+    while (T--) {
+        // printf("Case %d:\n", t);
+        readInput();
+        solver();
     }
     return 0;
 }
